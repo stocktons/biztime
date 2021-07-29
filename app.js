be25ee03dab318c8c7f5e23742e1ab28d8f36d07
stocktons <sarah@thanksforallthe.fish>
2021-07-29 19:27:12 -0400

    get one invoice route

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const express = require("express");
 
 const app = express();
 const companiesRoutes = require("./routes/companies");
+const invoiceRoutes = require("./routes/invoices")
 const { NotFoundError } = require("./expressError");
 
 app.use(express.json());
 
 app.use("/companies", companiesRoutes);
+app.use("/invoices", invoiceRoutes); 
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,1 +1,51 @@
+/** Routes about invoices.  */
+const express = require("express")
+const { NotFoundError, BadRequestError } = require("../expressError")
+
 const db = require("../db");
+const router = new express.Router();
+
+/** GET / - returns `{invoices: [{id, comp_code}]}` */
+
+router.get("/", async function (req, res, next) {
+    const results = await db.query("SELECT id, comp_code FROM invoices");
+    const invoices = results.rows;
+
+    return res.json({ invoices });
+});
+
+/** GET /[id] - return data about one invoice: 
+ * `{invoice: {id, amt, paid, add_date, paid_date}, 
+ * company: {code, name, description}}` */
+
+router.get("/:id", async function (req, res, next) {
+
+    const id = req.params.id;
+    const iResults = await db.query(
+        `SELECT id, comp_code, amt, paid, add_date, paid_date
+        FROM invoices
+        WHERE id = $1`, [id]);
+    const comp_code = iResults.rows[0].comp_code;
+    const invoice = { ...iResults.rows[0], comp_code: undefined };
+
+    const cResults = await db.query(
+        `SELECT code, name, description
+        FROM companies
+        WHERE code = $1`, [comp_code]);
+    const company = cResults.rows[0];
+
+    // our original solution: okay, but not as compact
+    // const cResults = await db.query( 
+    //     `SELECT code, name, description
+    //     FROM companies
+    //     JOIN invoices
+    //     ON invoices.comp_code = companies.code
+    //     WHERE invoices.id = $1`, [id]);
+    // const company = cResults.rows[0];
+
+    return res.json({ invoice, company });
+});
+
+
+
+module.exports = router;
